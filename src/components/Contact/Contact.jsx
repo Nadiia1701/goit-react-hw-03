570502fb00b0607803e223ac74989d41e0d6ef50
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-04-13 15:56:34 -0400

    summary3

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,15 @@
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { ImAddressBook } from "react-icons/im";
+import css from "./Contact.module.css"
+
 export default function Contact({ data: { id, name, number }, onDelete }) {
     return (
-        <div>
-            <p>{name}</p>
-            <p>{number}</p>
-            <button onClick={() => onDelete(id)}>Delete</button>
+        <div className={css.container} >
+            <div className={css.textContainer}>
+                <p><ImAddressBook /> {name}</p>
+                <p><FaPhoneSquareAlt /> {number}</p>
+            </div>
+            <button className={css.btn} onClick={() => onDelete(id)}>Delete</button>
         </div>
     );
 }
